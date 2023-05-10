bada794204282371d5743491545c0d9c5ac4bddf
Philip M. Gollucci (local) <pgollucci@jlhewitt.local>
2023-05-10 11:21:30 -0400

    chore: Initial commit.

---
+++ init.zsh
@@ -0,0 +1,48 @@
+# shellcheck shell=bash
+######################################################################
+#<
+#
+# Function: p6df::modules::p6python::deps()
+#
+#>
+######################################################################
+p6df::modules::p6python::deps() {
+  ModuleDeps=(
+    p6m7g8-dotfiles/p6common
+  )
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::p6python::init()
+#
+#  Environment:	 P6_DFZ_SRC_P6M7G8_DOTFILES_DIR
+#>
+######################################################################
+p6df::modules::p6python::init() {
+
+  p6_python_init $P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6python
+
+  p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: p6_python_init(dir)
+#
+#  Args:
+#	dir -
+#
+#  Environment:	 PYTHONPATH
+#>
+######################################################################
+p6_python_init() {
+    local dir="$1"
+
+    p6_path_if "$dir/bin"
+    p6_python_path_if "$dir/lib"
+
+    p6_return_void
+}
